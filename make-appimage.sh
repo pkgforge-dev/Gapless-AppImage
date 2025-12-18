2fdfcdb3c24f04f9379c62d597fa9e184e80a13a
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-18 12:45:21 +0100

    Add `GTK_CLASS_FIX` for StartupWMClass fix on Wayland

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,8 @@ export DESKTOP=/usr/share/applications/com.github.neithern.g4music.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1 # Pipewire audio backend is removed in later versions, but I'll let it stay here in case it gets added again
 export DEPLOY_GSTREAMER=1
-export STARTUPWMCLASS=g4music # For Wayland, this is 'com.github.neithern.g4music', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
+export STARTUPWMCLASS=g4music
+export GTK_CLASS_FIX=1
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
 quick-sharun /usr/bin/g4music
